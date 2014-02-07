2810958491b2a143b1f50013d109200988d6a7ff
sunsoft22 <skrithi1@umbc.edu>
2014-02-07 09:21:25 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Skrithi1DemoApp::Application.config.secret_key_base = '7c2f8ed840a88c1d89d9a1f90f853de1aa5aebe7689713a5260b190fb7334b80b1b5646b44c3a0d5ee6dde12e5166018870df71d07110a3174fc5af954801848'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Skrithi1DemoApp::Application.config.session_store :cookie_store, key: '_skrithi1_demo_app_session'
